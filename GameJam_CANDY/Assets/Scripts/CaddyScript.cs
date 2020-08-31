10b56b48abaa6c895c77d73fd38b108dd018f757
Christopher <cborchardt95@gmail.com>
2020-08-31 18:17:54 +0200

    Moveset vom Spieler erweitert
    Spieler kann jetzt mit der Leertaste eine Hechtsprungrolle machen. Zudem wurden die Animationen für das Springen und Fallen hinzugefügt

---
+++ GameJam_CANDY/Assets/Scripts/CaddyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaddyScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(rb.velocity.x) > 0.1f) sprite.flipX = rb.velocity.x < 0;
+    }
+}
--- GameJam_CANDY/Assets/Scripts/PlayerScript.cs
+++ GameJam_CANDY/Assets/Scripts/PlayerScript.cs
@@ -20,8 +20,12 @@ public class PlayerScript : MonoBehaviour
     
     /// <summary> Zähler, der bestimmt wie lange der Char noch springen kann </summary>
     private float jumpCounter;
-    /// <summary> Faktor, mit dem die Geschwindigkeit des Char veringert wird </summary>
-    private Vector2 dampingVec;
+    /// <summary> Wenn wahr, dann wird gerade eine Hechtrolle ausgeführt </summary>
+    private bool blockDive;
+    /// <summary> Wenn wahr, dann ist der Char in der Luft, wenn falsch, dann ist er auf dem Boden </summary>
+    private bool inAir;
+    /// <summary> Mask für den Raycast, um Bodenkontakt festzustellen </summary>
+    private int rMask = (1<<10) | (1<<15);//Kontakt mit Ground und Einkaufswagen
 
     // Start is called before the first frame update
     void Start()
@@ -36,7 +40,23 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
 
-        //Simple Steuerung des Spielers:
+        //Steuerung des Spielers:
+
+        //Checke ob in der Luft:
+        inAir = !Physics2D.Raycast(transform.position, Vector2.down, 2.6f, rMask).collider;
+
+        //Checke Hechtrolle:
+        if ((Input.GetKey(KeyCode.Space) && !inAir) || blockDive)
+        {
+            jumpCounter = 0;
+            if (!blockDive)
+            {
+                blockDive = true;
+                float factor = Mathf.Sign(transform.localScale.x) * 1.5f;
+                StartCoroutine(DoDive(factor));
+            }
+            return;
+        }
 
         //Checke Sprung:
         if(Input.GetKey(KeyCode.W) && jumpCounter > 0)
@@ -47,19 +67,46 @@ public class PlayerScript : MonoBehaviour
         }
 
         //Checke Bewegung:
-        if(Input.GetKey(KeyCode.D) == Input.GetKey(KeyCode.A)) { rb.velocity *= new Vector2(1 - speedDamping ,1); anim.SetBool("running", false); return; }
+        if(Input.GetKey(KeyCode.D) == Input.GetKey(KeyCode.A))
+        {
+            rb.velocity *= new Vector2(1 - speedDamping ,1);
+            anim.SetBool("running", false);
+        }
+        else
+        {
+            anim.SetBool("running", true);
+            float dir = Input.GetKey(KeyCode.D) ? 1 : -1;
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * dir, transform.localScale.y, 1);
 
-        anim.SetBool("running", true);
-        float dir = Input.GetKey(KeyCode.D) ? 1 : -1;
-        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * dir, transform.localScale.y, 1);
+            if (Mathf.Abs(rb.velocity.x) < maxSpeed || Mathf.Sign(rb.velocity.x) != dir )
+                rb.velocity += Vector2.right * dir * acceleration * Time.deltaTime;
+        }
 
-        if (Mathf.Abs(rb.velocity.x) < maxSpeed || Mathf.Sign(rb.velocity.x) != dir )
-            rb.velocity += Vector2.right * dir * acceleration;// * Time.deltaTime;
+        //Fallanimation:
+        anim.SetBool("inAir", inAir);
+        if (inAir)
+            anim.SetBool("falling", rb.velocity.y < 0);
+        else
+            jumpCounter = jumpPower;
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    /// <summary>
+    /// Führt eine Hechtrolle in die richtung entsprechend factor aus
+    /// </summary>
+    /// <param name="factor">hechtrolle bei positiv, nach rechts und bei negativ, nach links</param>
+    /// <returns></returns>
+    private IEnumerator DoDive(float factor)
     {
-        //Debug.Log("Refill jumpPower");
-        jumpCounter = jumpPower;
+        anim.SetInteger("dive", (int)Mathf.Sign(factor));
+        anim.SetTrigger("doDive");
+        float t = 0;
+        while(t < 0.5f)
+        {
+            t += Time.fixedDeltaTime;
+            rb.position += Vector2.right * factor * maxSpeed * 1.5f * Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        blockDive = false;
+        yield break;
     }
 }
