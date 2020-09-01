626aa078d7ce02fd644866761173fe1fda199d77
Christopher <cborchardt95@gmail.com>
2020-09-01 23:43:00 +0200

    Collectables- effects added
    - collectables emit light
    - changed hovering of collectables to an animation

---
+++ GameJam_CANDY/Assets/Scripts/CollScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator PulsingLight()
+    {
+        while (true)
+        {
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ GameJam_CANDY/Assets/Scripts/PulsingLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class PulsingLight : MonoBehaviour
+{
+    public float speed;
+    public float min, max;
+
+    Light2D light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        light.intensity = min + (Mathf.Sin(Time.fixedTime * speed) + 1) * 0.5f * (max - min);
+    }
+}
